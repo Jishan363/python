5d3ae90d569e6654cc5a779252fbc8d8b95f782a
Jishan363 <jishanbalekundri@gmail.com>
2025-07-11 22:37:19 +0530

    Update Assignment Task 1.py

--- Assignment Task 1.py
+++ Assignment Task 1.py
@@ -1,1 +1,11 @@
-
+# TASK1: PERFORM BASIC MATHEMATICAL OPERATIONS
+a=int(input('Enter the first number: '))
+b=int(input('Enter the Second number: '))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition: ',c)
+print('Subtraction: ',d)
+print('Multiplication: ',e)
+print('Division: ',f)
