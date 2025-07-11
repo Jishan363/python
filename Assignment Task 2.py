160fce2a6d02d39478f59da80116e999d67d0923
Jishan363 <jishanbalekundri@gmail.com>
2025-07-11 22:39:18 +0530

    Update Assignment Task 2.py

--- Assignment Task 2.py
+++ Assignment Task 2.py
@@ -1,1 +1,5 @@
-
+# TASK2: CREATE A PERSONALIZED GREETING
+a=str(input("Enter your first Name: "))
+b=str(input("Enter your Last Name: "))
+c=a+" "+b
+print("HELLO,"+c+"! Welcome to the python program.")
